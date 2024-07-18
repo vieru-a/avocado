f1f8098f73f2586a00f3aaed1f6072eac588a66c
vieru-a <vierua2200@gmail.com>
2024-07-18 20:02:49 +0300

    Changed run settings

--- avocado-fastapi/main.py
+++ avocado-fastapi/main.py
@@ -1,8 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
+from core.config import settings
+
 app = FastAPI()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", reload=True)
+    uvicorn.run(
+        app="main:app", host=settings.run.host, port=settings.run.port, reload=True
+    )
