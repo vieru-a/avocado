ffe2235b22c070410fa331cd240f7c0048091440
vieru-a <vierua2200@gmail.com>
2024-07-25 14:41:20 +0300

    Add db_helper dispose to main app

--- avocado-fastapi/main.py
+++ avocado-fastapi/main.py
@@ -1,12 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
 
-app = FastAPI()
+from database.db_helper import db_helper
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     uvicorn.run(
-        app="main:app", host=settings.run.host, port=settings.run.port, reload=True
+        app="main:main_app", host=settings.run.host, port=settings.run.port, reload=True
     )
