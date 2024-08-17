6de1cf7046d0b02f6f2c307a6f1f92fbf7833446
vieru-a <vierua2200@gmail.com>
2024-08-17 22:34:07 +0300

    Created relationships between user & wish

--- avocado-fastapi/database/user.py
+++ avocado-fastapi/database/user.py
@@ -1,8 +1,15 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, Relationship
 
 from database import BaseModel
+
+if TYPE_CHECKING:
+    from .wish import Wish
 
 
 class User(BaseModel):
     username: Mapped[str] = mapped_column(String(32), unique=True)
+
+    wish: Mapped[list["Wish"]] = Relationship(back_populates="user")
--- avocado-fastapi/database/wish.py
+++ avocado-fastapi/database/wish.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey, String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, Relationship
 
 from database import BaseModel
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Wish(BaseModel):
@@ -14,3 +19,5 @@ class Wish(BaseModel):
     holiday: Mapped[str]
 
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    user: Mapped["User"] = Relationship(back_populates="wish")
