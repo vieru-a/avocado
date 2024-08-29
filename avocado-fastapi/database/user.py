4ea4e8144ac7c8f754fe0ce3677b6c741d3dd737
vieru-a <vierua2200@gmail.com>
2024-08-29 21:39:34 +0300

    Changed product user & added IdIntPkMixin

--- avocado-fastapi/database/user.py
+++ avocado-fastapi/database/user.py
@@ -1,15 +1,18 @@
 from typing import TYPE_CHECKING
+from fastapi_users.db import SQLAlchemyBaseUserTable
 
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, Relationship
 
 from database import BaseModel
+from .mixins.id_int_pk import IdIntPkMixin
 
 if TYPE_CHECKING:
     from .wish import Wish
 
 
-class User(BaseModel):
-    username: Mapped[str] = mapped_column(String(32), unique=True)
+class User(BaseModel, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
+    first_name: Mapped[str] = mapped_column(String(32), unique=False)
+    last_name: Mapped[str] = mapped_column(String(32), unique=False)
 
     wish: Mapped[list["Wish"]] = Relationship(back_populates="user")
