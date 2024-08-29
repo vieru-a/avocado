12d4578b4c057d5216e2de5d60d004fdd9717059
vieru-a <vierua2200@gmail.com>
2024-08-29 21:38:54 +0300

    Changed product model & added IdIntPkMixin

--- avocado-fastapi/database/product.py
+++ avocado-fastapi/database/product.py
@@ -1,11 +1,15 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from database import BaseModel
+from database.mixins.id_int_pk import IdIntPkMixin
 
 
-class Product(BaseModel):
-
-    name: Mapped[str]
-    description: Mapped[str]
-    category: Mapped[str]
+class Product(BaseModel, IdIntPkMixin):
+    title: Mapped[str] = mapped_column(String(100), unique=False)
+    description: Mapped[str] = mapped_column(
+        Text,
+        default=None,
+        server_default=None,
+    )
     price: Mapped[int]
