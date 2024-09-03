c5ed9e5c5e3253d85b1957811a528ce35afa07d9
vieru-a <vierua2200@gmail.com>
2024-09-03 20:04:17 +0300

    Added method for user database

--- avocado-fastapi/database/user.py
+++ avocado-fastapi/database/user.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, Relationship
@@ -9,6 +10,7 @@ from .mixins.id_int_pk import IdIntPkMixin
 
 if TYPE_CHECKING:
     from .wish import Wish
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(BaseModel, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
@@ -16,3 +18,7 @@ class User(BaseModel, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
     last_name: Mapped[str] = mapped_column(String(32), unique=False)
 
     wish: Mapped[list["Wish"]] = Relationship(back_populates="user")
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
