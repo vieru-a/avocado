38afd5eff5cd56ab054b9ee594e1ab8d7de077d6
vieru-a <vierua2200@gmail.com>
2024-09-10 19:36:24 +0300

    Created authentication backend

---
+++ avocado-fastapi/api/dependencies/backend.py
@@ -0,0 +1,14 @@
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    BearerTransport,
+    JWTStrategy,
+)
+
+from database.authentication.transport import bearer_transport
+from .strategy import get_database_strategy
+
+authentication_backend = AuthenticationBackend(
+    name="access-tokens-db",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
+)
