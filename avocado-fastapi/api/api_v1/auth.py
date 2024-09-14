f3bd2deddc3e7a5537d297694f8971f8380c51a9
vieru-a <vierua2200@gmail.com>
2024-09-14 14:49:11 +0300

    Included auth router from fastapi -users

--- avocado-fastapi/api/api_v1/auth.py
+++ avocado-fastapi/api/api_v1/auth.py
@@ -2,7 +2,14 @@ from fastapi import APIRouter
 
 from core.config import settings
 
+from api.api_v1.fastapi_users_router import fastapi_users_router
+from api.dependencies.authentication.backend import authentication_backend
+
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    router=fastapi_users_router.get_auth_router(authentication_backend),
 )
