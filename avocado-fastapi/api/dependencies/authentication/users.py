df87870a1442fc7b664f74ae6f7eecfaf89652d3
vieru-a <vierua2200@gmail.com>
2024-09-16 19:51:05 +0300

    Fixed db_helper import for users

--- avocado-fastapi/api/dependencies/authentication/users.py
+++ avocado-fastapi/api/dependencies/authentication/users.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 from fastapi import Depends
 
 from database import User
-from database.db_helper import DatabaseHelper
+from database.db_helper import db_helper
 
 
 if TYPE_CHECKING:
@@ -10,6 +10,6 @@ if TYPE_CHECKING:
 
 
 async def get_users_db(
-    session: "AsyncSession" = Depends(DatabaseHelper.session_getter),
+    session: "AsyncSession" = Depends(db_helper.session_getter),
 ):
     yield User.get_db(session=session)
