7408ce4f690f256ba087caf0d6e4293d2e2cd0ad
vieru-a <vierua2200@gmail.com>
2024-09-16 19:50:59 +0300

    Fixed db_helper import for access_tokens

--- avocado-fastapi/api/dependencies/authentication/access_tokens.py
+++ avocado-fastapi/api/dependencies/authentication/access_tokens.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 from fastapi import Depends
 
 from database import AccessToken
-from database.db_helper import DatabaseHelper
+from database.db_helper import db_helper
 
 
 if TYPE_CHECKING:
@@ -10,6 +10,6 @@ if TYPE_CHECKING:
 
 
 async def get_access_tokens_db(
-    session: "AsyncSession" = Depends(DatabaseHelper.session_getter),
+    session: "AsyncSession" = Depends(db_helper.session_getter),
 ):
     yield AccessToken.get_db(session=session)
