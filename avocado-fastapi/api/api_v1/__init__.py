17d55c079884303b79c45c072b06cd08642be7d4
vieru-a <vierua2200@gmail.com>
2024-09-18 19:40:56 +0300

    Added http bearer dependence for tokens

--- avocado-fastapi/api/api_v1/__init__.py
+++ avocado-fastapi/api/api_v1/__init__.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from core.config import settings
 from .auth import router as auth_router
 from .products.views import router as products_router
 from .users import router as users_router
 
+http_bearer = HTTPBearer(auto_error=False)
+
 router = APIRouter(
     prefix=settings.api.v1.prefix,
+    dependencies=[Depends(http_bearer)],
 )
 
 router.include_router(router=auth_router)
